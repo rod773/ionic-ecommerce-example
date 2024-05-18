552212e1f46aea8524fe27a19cfc90c32e9ada05
rod773 <rodrigoarcadio1@gmail.com>
2024-05-18 09:04:19 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,73 +1,59 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { setupIonicReact } from '@ionic/react';
-import Home from './pages/Home';
+import { Navigate, Route, BrowserRouter, Routes } from "react-router-dom";
+import { IonApp, IonRouterOutlet } from "@ionic/react";
+//import { IonReactRouter } from "@ionic/react-router";
+import { setupIonicReact } from "@ionic/react";
+import Home from "./pages/Home";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import { useEffect } from 'react';
-import { fetchData } from './data/fetcher';
-import CategoryProducts from './pages/CategoryProducts';
-import Product from './pages/Product';
-import FavouriteProducts from './pages/FavouriteProducts';
-import CartProducts from './pages/CartProducts';
+import "./theme/variables.css";
+import { useEffect } from "react";
+import { fetchData } from "./data/fetcher";
+import CategoryProducts from "./pages/CategoryProducts";
+import Product from "./pages/Product";
+import FavouriteProducts from "./pages/FavouriteProducts";
+import CartProducts from "./pages/CartProducts";
 
 setupIonicReact({});
 
 const App = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	useEffect(() => {
+  return (
+    <IonApp>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-		fetchData();
-	}, []);
+          <Route path="/favourites" element={<FavouriteProducts />} />
 
-	return (
-		<IonApp>
-			<IonReactRouter>
-				<IonRouterOutlet>
-					<Route path="/" exact={true}>
-						<Redirect to="/home" />
-					</Route>
-					<Route path="/home" exact={true}>
-						<Home />
-					</Route>
+          <Route path="/cart" element={<CartProducts />} />
 
-					<Route path="/favourites" exact>
-						<FavouriteProducts />
-					</Route>
+          <Route path="/category/:slug" element={<CategoryProducts />} />
 
-					<Route path="/cart" exact>
-						<CartProducts />
-					</Route>
-
-					<Route path="/category/:slug" exact>
-						<CategoryProducts />
-					</Route>
-
-					<Route path="/category/:slug/:id" exact>
-						<Product />
-					</Route>
-				</IonRouterOutlet>
-			</IonReactRouter>
-		</IonApp>
-	);
-}
+          <Route path="/category/:slug/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+    </IonApp>
+  );
+};
 
 export default App;
